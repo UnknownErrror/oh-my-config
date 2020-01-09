8e66747bb13fc128baed3db44bb58304144c8ae8
UnknownErrror <herobrine135111@gmail.com>
2020-01-09 07:09:54 +0300

    .

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -1,6 +1,9 @@
 # fixme - the load process here seems a bit bizarre
 zmodload -i zsh/complist
 
+autoload -U compinit
+compinit -i
+
 WORDCHARS=''
 
 setopt ALWAYS_TO_END
@@ -42,7 +45,54 @@ zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-dir
 zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion::complete:*' cache-path $ZSH_CACHE_DIR
 
+
+zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
+zstyle ':completion:*' menu select=1 _complete _ignored _approximate
+zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
+
+# allow one error for every three characters typed in approximate completer
+zstyle ':completion:*:approximate:*' max-errors 3
+
+# formatting and messages
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:matches' group yes
+zstyle ':completion:*:options' description yes
+zstyle ':completion:*:descriptions' format $'\e[01;33m -- %d --\e[0m'
+zstyle ':completion:*:messages' format $'\e[01;35m -- %d --\e[0m'
+zstyle ':completion:*:warnings' format $'\e[01;31m -- No Matches Found --\e[0m'
+zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*:options' auto-description '%d'
+
+# command completion: highlight matching part of command, and 
+zstyle -e ':completion:*:-command-:*:commands' list-colors 'reply=( '\''=(#b)('\''$words[CURRENT]'\''|)*-- #(*)=0=38;5;45=38;5;136'\'' '\''=(#b)('\''$words[CURRENT]'\''|)*=0=38;5;45'\'' )'
+
+# This is needed to workaround a bug in _setup:12, causing almost 2 seconds delay for bigger LS_COLORS
+# UPDATE: not sure if this is required anymore, with the -command- style above.. keeping it here just to be sure
+zstyle ':completion:*:*:-command-:*' list-colors ''
+
+# use LS_COLORS for file coloring
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# show command short descriptions, too
+zstyle ':completion:*' extra-verbose yes
+# make them a little less short, after all (mostly adds -l option to the whatis calll)
+zstyle ':completion:*:command-descriptions' command '_call_whatis -l -s 1 -r .\*; _call_whatis -l -s 6 -r .\* 2>/dev/null'
+
+# for sudo kill, show all processes except childs of kthreadd (ie, kernel
+# threads), which is assumed to be PID 2. otherwise, show user processes only.
+zstyle -e ':completion:*:*:kill:*:processes' command '[[ $BUFFER == sudo* ]] && reply=( "ps --forest -p 2 --ppid 2 --deselect -o pid,user,cmd" ) || reply=( ps x --forest -o pid,cmd )'
+zstyle ':completion:*:processes-names' command 'ps axho command' 
+
+## add colors to processes for kill completion
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+
+zle -C complete-history complete-word _generic
+zstyle ':completion:complete-history:*' completer _history
+
+
 # Don't complete uninteresting users
+zstyle ':completion:*:functions' ignored-patterns '_*'
 zstyle ':completion:*:*:*:users' ignored-patterns \
 	adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
 	clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
